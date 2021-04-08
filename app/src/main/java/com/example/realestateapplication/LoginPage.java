882e351d90167105541360631cc50159e02c895d
Minjae Lee <1793875@edu.vaniercollege.qc.ca>
2021-04-08 17:30:05 -0400

    Login page imcomplete
    Login are created, but registration is missing.

---
+++ app/src/main/java/com/example/realestateapplication/LoginPage.java
@@ -0,0 +1,17 @@
+package com.example.realestateapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+    }
+
+    //for registration:
+    //https://www.youtube.com/watch?v=ARezg1D9Zd0
+}
