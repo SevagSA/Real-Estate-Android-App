8954872e91c38d18ef5910c94c34e39caf15da83
Minjae Lee <1793875@edu.vaniercollege.qc.ca>
2021-04-11 14:07:29 -0400

    Agent List and Search Result
    Two lists are made

--- app/src/main/java/com/example/realestateapplication/Controllers/LoginActivity.java
+++ app/src/main/java/com/example/realestateapplication/Controllers/LoginActivity.java
@@ -3,17 +3,35 @@ package com.example.realestateapplication.Controllers;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.realestateapplication.R;
+import com.example.realestateapplication.RegistrationDialog;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button regButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        regButton = findViewById(R.id.registerButton);
+        regButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog();
+            }
+        });
+    }
+
+    public void openDialog() {
+        RegistrationDialog regDialog = new RegistrationDialog();
+        regDialog.show(getSupportFragmentManager(), "registration dialog");
     }
 
     //for registration:
     //https://www.youtube.com/watch?v=ARezg1D9Zd0
+    //9:11
 }
---
+++ app/src/main/java/com/example/realestateapplication/RegistrationDialog.java
@@ -0,0 +1,45 @@
+package com.example.realestateapplication;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class RegistrationDialog extends AppCompatDialogFragment {
+    private EditText regUsername;
+    private EditText regPassword;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.layout_registration, null);
+
+        builder.setView(view)
+                .setTitle("Registrate")
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+
+        regUsername = view.findViewById(R.id.reg_username);
+        regPassword = view.findViewById(R.id.reg_password);
+
+        return builder.create();
+    }
+}
